7a5e04bb39d6b02b3e9e8dd40c2c51351edc0427
James Santucci <james.santucci@gmail.com>
2019-03-06 15:35:41 -0500

    Revert upgrade, add mask to mosaic service (broken)

--- app-backend/backsplash-server/src/main/scala/com/rasterfoundry/backsplash/ToolStoreImplicits.scala
+++ app-backend/backsplash-server/src/main/scala/com/rasterfoundry/backsplash/ToolStoreImplicits.scala
@@ -55,7 +55,9 @@ class ToolStoreImplicits[HistStore](mosaicImplicits: MosaicImplicits[HistStore],
       }
     } yield {
       val decoded = executionParams.as[MapAlgebraAST] match {
-        case Right(x) => x
+        case Right(x) =>
+          println(s"AST in tile server is: ${x}")
+          x
         case Left(e) =>
           logger.error(e.getMessage)
           throw MetadataException(s"Could not decode AST for $analysisId")
